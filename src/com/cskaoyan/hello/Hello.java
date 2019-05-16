71e0e9287ff490f57ffe3608badcc2e92d25023e
GYP <ebob@>
2019-05-16 23:13:38 +0800

    22222222在idea中修改了输出代码

--- src/com/cskaoyan/hello/Hello.java
+++ src/com/cskaoyan/hello/Hello.java
@@ -3,5 +3,6 @@ package com.cskaoyan.hello;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello cat");
+        System.out.println("到此一游");
     }
 }

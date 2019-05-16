b3d6c920819cafdd03c703d005684c2030b51b80
GYP <ebob@>
2019-05-16 23:21:29 +0800

    22222222在idea中修改了输出代

--- src/com/cskaoyan/hello/Hello.java
+++ src/com/cskaoyan/hello/Hello.java
@@ -3,6 +3,6 @@ package com.cskaoyan.hello;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello cat");
-        System.out.println("到此一游 ");
+        System.out.println("are u ok?");
     }
 }

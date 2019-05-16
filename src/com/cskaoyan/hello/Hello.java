da404eb9f5cfd29e6d7312f0cf399f31861d566c
GYP <ebob@>
2019-05-16 23:28:32 +0800

    来源于23333

--- src/com/cskaoyan/hello/Hello.java
+++ src/com/cskaoyan/hello/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello cat");
         System.out.println("are u ok?");
+		System.out.println("不ok");
     }
 }
